79f8a2eec79724f64656ed5a209fcdada1834ba5
rishithatcs <101921351+rishithatcs@users.noreply.github.com>
2026-03-02 12:06:09 +0530

    Add LoginRequest DTO for authentication

---
+++ src/main/java/com/elms/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.elms.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
